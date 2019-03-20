66862540ca83b3538ea7c9c6aa0020126e2ff9bd
takuuuu517 <ryoryoryoasa@gmail.com>
2019-03-19 19:30:46 -0600

    hello

--- Archive/app_with_handler.py
+++ Archive/app_with_handler.py
@@ -21,7 +21,7 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    InvalidSignatureError 
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
